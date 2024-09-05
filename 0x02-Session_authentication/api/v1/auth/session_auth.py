17cc0a3d78e1ebf6cfb3c6de2d2f90c01e9dd37e
Mohammed <6bouass9@gmail.com>
2024-09-05 23:15:30 +0100

    push task 1

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,6 +21,7 @@ class SessionAuth(Auth):
             self.user_id_by_session_id[session_id] = user_id
             return session_id
 
+
 def user_id_for_session_id(self, session_id: str = None) -> str:
     """Retrieves the user id of the user associated with
     a given session id.
@@ -28,11 +29,13 @@ def user_id_for_session_id(self, session_id: str = None) -> str:
     if type(session_id) is str:
         return self.user_id_by_session_id.get(session_id)
 
+
 def current_user(self, request=None) -> User:
     """Retrieves the user associated with the request.
     """
     user_id = self.user_id_for_session_id(self.session_cookie(request))
     return User.get(user_id)
+
 
 def destroy_session(self, request=None):
     """Destroys an authenticated session.
